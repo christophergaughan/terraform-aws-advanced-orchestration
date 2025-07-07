322d616dea71752e79714078af3a3c27eafba478
Christopher Gaughan <58716009+christophergaughan@users.noreply.github.com>
2025-07-06 22:55:49 -0400

    Update Lambda module with SageMaker env var and correct handler

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,9 +1,15 @@
 resource "aws_lambda_function" "this" {
-  function_name = var.function_name
-  role          = var.role_arn
-  handler       = "index.handler"
-  runtime       = "python3.9"
-  filename      = var.filename
+  function_name    = var.function_name
+  role             = var.role_arn
+  handler          = "lambda_function.lambda_handler" # <- update to match  Python file
+  runtime          = "python3.9"
+  filename         = var.filename
   source_code_hash = filebase64sha256(var.filename)
+
+  environment {
+    variables = {
+      SAGEMAKER_ENDPOINT_NAME = "my-sagemaker-endpoint" # Replace with real endpoint name later
+    }
+  }
 }
 
